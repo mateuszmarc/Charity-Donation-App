71738fe855cee5e2bfb37a9cd937bdbe05e8b140
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-20 09:43:46 +0000

    Comment out security dependency for development purposes

---
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/home/HomeController.java
@@ -0,0 +1,35 @@
+package pl.mateuszmarcyk.charity_donation_app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/confirm")
+    public String confirmation() {
+        return "form-confirmation";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+}

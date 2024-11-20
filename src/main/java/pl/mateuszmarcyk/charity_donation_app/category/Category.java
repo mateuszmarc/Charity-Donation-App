c61e53f15793eb660bd72e82ab1507ff01cc74ea
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-20 10:41:27 +0000

    Create Donation entity Update Category and Institution entities: - Add needed relationships

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/category/Category.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/category/Category.java
@@ -2,6 +2,9 @@ package pl.mateuszmarcyk.charity_donation_app.category;
 
 import jakarta.persistence.*;
 import lombok.*;
+import pl.mateuszmarcyk.charity_donation_app.donation.Donation;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,6 +22,22 @@ public class Category {
 
     @Column(name = "name")
     private String name;
+
+    @ManyToMany(
+            targetEntity = Donation.class,
+            cascade = {
+                    CascadeType.DETACH,
+                    CascadeType.MERGE,
+                    CascadeType.PERSIST,
+                    CascadeType.REFRESH
+            },
+            fetch = FetchType.LAZY
+    )
+    @JoinTable(name = "donations_categories",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "donation_id")
+    )
+    private List<Donation> donations;
 
 
 }
--- src/main/java/pl/mateuszmarcyk/charity_donation_app/institution/Institution.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/institution/Institution.java
@@ -2,6 +2,9 @@ package pl.mateuszmarcyk.charity_donation_app.institution;
 
 import jakarta.persistence.*;
 import lombok.*;
+import pl.mateuszmarcyk.charity_donation_app.donation.Donation;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,4 +25,17 @@ public class Institution {
 
     @Column(name = "description")
     private String description;
+
+    @OneToMany(
+            targetEntity = Donation.class,
+            mappedBy = "institution",
+            cascade = {
+                    CascadeType.DETACH,
+                    CascadeType.MERGE,
+                    CascadeType.PERSIST,
+                    CascadeType.REFRESH
+            },
+            fetch = FetchType.LAZY
+    )
+    private List<Donation> donations;
 }

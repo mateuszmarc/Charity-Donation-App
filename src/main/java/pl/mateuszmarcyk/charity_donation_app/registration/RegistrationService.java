0ab45f36a7a748cc801f01646619967e5f962a3e
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-20 16:44:56 +0000

    Remove AllArgsConstructor annotation

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/registration/RegistrationService.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/registration/RegistrationService.java
@@ -1,13 +1,11 @@
 package pl.mateuszmarcyk.charity_donation_app.registration;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
-@AllArgsConstructor
 @Service
 public class RegistrationService {
 

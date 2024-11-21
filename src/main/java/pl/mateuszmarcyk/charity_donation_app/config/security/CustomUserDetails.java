f50f1b24d7fc6001a56471973128cc9adb6ae06e
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-21 10:36:34 +0000

    Create CustomUserDetails and CustomUserDetailsService

---
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/config/security/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package pl.mateuszmarcyk.charity_donation_app.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pl.mateuszmarcyk.charity_donation_app.user.User;
+import pl.mateuszmarcyk.charity_donation_app.usertype.UserType;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<UserType> userTypes = user.getUserTypes();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        userTypes.forEach(type -> authorities.add(new SimpleGrantedAuthority(type.getRole())));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}

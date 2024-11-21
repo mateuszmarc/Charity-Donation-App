5d6c98093576ce09fb512a43a5aae2aa02741a6a
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-21 10:50:32 +0000

    Uncomment spring-boot dependency in pom.xml file. Create CustomAuthenticationSuccessHandler Create AuthenticationProvider Bean in WebSecurityConfig

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/config/WebSecurityConfig.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/config/WebSecurityConfig.java
@@ -1,14 +1,35 @@
 package pl.mateuszmarcyk.charity_donation_app.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.mateuszmarcyk.charity_donation_app.config.security.CustomAuthenticationSuccessHandler;
+import pl.mateuszmarcyk.charity_donation_app.config.security.CustomUserDetailsService;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+        return authenticationProvider;
+    }
 }

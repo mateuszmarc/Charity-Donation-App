7d3164797b59992ef7a920f9801888db62efa8ec
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-21 14:33:39 +0000

    Create DonationRepository: - Create countAll and countAllBags methods
    Create DonationService:
    - Create countAll and countAllBags methods
    
    Create UserProfileService

---
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationService.java
@@ -0,0 +1,4 @@
+package pl.mateuszmarcyk.charity_donation_app.donation;
+
+public class DonationService {
+}
---
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/userprofile/UserProfileService.java
@@ -0,0 +1,4 @@
+package pl.mateuszmarcyk.charity_donation_app.userprofile;
+
+public class UserProfileService {
+}

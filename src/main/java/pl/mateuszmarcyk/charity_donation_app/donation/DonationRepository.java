932382e2a450dd1eadfc55ff37664a122c587dee
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-21 14:41:29 +0000

    Create DonationRepository: - Create countAll and countAllBags methods
    Create DonationService:
    - Create countAll and countAllBags methods
    
    Create UserProfileService
    
    Add thymeleaf contifional statements to either display first name from UserProfile object or email from User object passed to the model.

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationRepository.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationRepository.java
@@ -1,4 +1,13 @@
 package pl.mateuszmarcyk.charity_donation_app.donation;
 
-public interface DonationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+    @Query("SELECT COUNT(d) FROM Donation d")
+    public long countAll();
+
+    @Query("SELECT SUM(d.quantity) FROM Donation  d")
+    public long countAllBags();
 }
--- src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationService.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationService.java
@@ -1,4 +1,19 @@
 package pl.mateuszmarcyk.charity_donation_app.donation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class DonationService {
+
+    private final DonationRepository donationRepository;
+
+    public long countAllDonations() {
+        return donationRepository.countAll();
+    }
+
+    public Long countAllBags() {
+        return donationRepository.countAllBags();
+    }
 }

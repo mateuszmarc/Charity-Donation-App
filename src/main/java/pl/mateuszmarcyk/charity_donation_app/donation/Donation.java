a2974e0841acc215f9fc1de800edc6987213324d
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-21 21:01:42 +0000

    Add donation validation constraint messages in messages.properties Add validation constraint annotations to Donstion entity

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/Donation.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/Donation.java
@@ -1,6 +1,10 @@
 package pl.mateuszmarcyk.charity_donation_app.donation;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import pl.mateuszmarcyk.charity_donation_app.category.Category;
 import pl.mateuszmarcyk.charity_donation_app.institution.Institution;
@@ -23,27 +27,37 @@ public class Donation {
     @Column(name = "id")
     private Long id;
 
+    @NotNull(message = "${donation.quantity.notnull}")
+    @Min(value = 1, message = "{donation.quantity.min}")
     @Column(name = "quantity")
     private Integer quantity;
 
+    @NotNull(message = "{donation.street.notnull}")
     @Column(name = "street")
     private String street;
 
+    @NotNull(message = "{donation.city.notnull}")
     @Column(name = "city")
     private String city;
 
+    @NotNull(message = "{donation.zipCode.notnull}")
+    @Pattern(regexp = "[0-9]{2}-[0-9]{3}", message = "{donation.zipCode.pattern}")
     @Column(name = "zip_code")
     private String zipCode;
 
+    @Future(message = "{donation.pickUpDate.future}")
     @Column(name = "pick_up_date")
     private LocalDate pickUpDate;
 
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "{donation.pickUpTime.pattern}")
     @Column(name = "pick_up_time")
     private LocalTime pickUpTime;
 
     @Column(name = "pick_up_comment")
     private String pickUpComment;
 
+    @NotNull
+    @Pattern(regexp = "[0-9]{9}", message = "{donation.phoneNumber.pattern}")
     @Column(name = "phone_number")
     private String phoneNumber;
 

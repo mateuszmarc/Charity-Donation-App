7c5cea4e4d546961d955387fcf6319fe44a2b5e5
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-21 08:24:33 +0000

    Create ResendTokenEvent and ResendTokenEventListener Move email sending functionality to RegistrationMailSender bean and inject it into RegistrationCompleteEventListener and ResendTokenEventListener

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/event/listener/ResendTokenEventListener.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/event/listener/ResendTokenEventListener.java
@@ -5,35 +5,44 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-import pl.mateuszmarcyk.charity_donation_app.event.RegistrationCompleteEvent;
+import pl.mateuszmarcyk.charity_donation_app.event.ResendTokenEvent;
 import pl.mateuszmarcyk.charity_donation_app.registration.verificationtoken.VerificationToken;
 import pl.mateuszmarcyk.charity_donation_app.registration.verificationtoken.VerificationTokenService;
 import pl.mateuszmarcyk.charity_donation_app.user.User;
 import pl.mateuszmarcyk.charity_donation_app.util.RegistrationMailSender;
 
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
-public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
+public class ResendTokenEventListener implements ApplicationListener<ResendTokenEvent> {
 
     @Value("${token.valid.time}")
     private int tokenValidTime;
 
+    @Value("${spring.mail.username}")
+    private String appEmail;
+
+    private VerificationTokenService verificationTokenService;
     private final RegistrationMailSender registrationMailSender;
-    private final VerificationTokenService verificationTokenService;
 
     @Override
-    public void onApplicationEvent(RegistrationCompleteEvent event) {
+    public void onApplicationEvent(ResendTokenEvent event) {
 
         User user = event.getUser();
-        String token = UUID.randomUUID().toString();
+        VerificationToken oldVerificationToken = event.getOldToken();
+        String applicationUrl = event.getApplicationUrl();
 
-        VerificationToken verificationToken = new VerificationToken(token, user, tokenValidTime);
-        verificationTokenService.saveToken(verificationToken);
+        String newToken = UUID.randomUUID().toString();
+        oldVerificationToken.setExpirationTime(LocalDateTime.now().plusMinutes(tokenValidTime));
+        oldVerificationToken.setToken(newToken);
+        oldVerificationToken.setUser(user);
 
-        String url = event.getApplicationUrl() + "/register/verifyEmail?token=" + token;
+        verificationTokenService.saveToken(oldVerificationToken);
+
+        String url = applicationUrl + "/register/verifyEmail?token=" + newToken;
 
         try {
             registrationMailSender.sendVerificationEmail(user, url);
@@ -41,6 +50,4 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
             throw new RuntimeException(e);
         }
     }
-
-
 }

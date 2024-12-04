9bc282a8df36a33110ebd8cc9805e0a1543c6cac
mateuszmarc <mmarckiewicz@gmail.com>
2024-12-04 09:06:09 +0000

    Use AuthenticationPrincipal instead of SecurityContextHolder in DonationController

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationController.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/donation/DonationController.java
@@ -3,9 +3,7 @@ package pl.mateuszmarcyk.charity_donation_app.donation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import pl.mateuszmarcyk.charity_donation_app.category.Category;
 import pl.mateuszmarcyk.charity_donation_app.category.CategoryService;
+import pl.mateuszmarcyk.charity_donation_app.config.security.CustomUserDetails;
 import pl.mateuszmarcyk.charity_donation_app.institution.Institution;
 import pl.mateuszmarcyk.charity_donation_app.institution.InstitutionService;
 import pl.mateuszmarcyk.charity_donation_app.user.User;
@@ -38,55 +37,57 @@ public class DonationController {
     }
 
     @GetMapping
-    public String displayDonationForm(Model model) {
+    public String displayDonationForm(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (userDetails != null) {
 
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            String username = authentication.getName();
-
-            User user = userService.findUserByEmail(username);
+            User user = userDetails.getUser();
             UserProfile userProfile = user.getProfile();
+
+            model.addAttribute("user", user);
+            model.addAttribute("userProfile", userProfile);
 
             List<Category> allCategories = categoryService.findAll();
             List<Institution> allInstitutions = institutionService.findAll();
 
-            model.addAttribute("userProfile", userProfile);
-            model.addAttribute("user", user);
             model.addAttribute("donation", new Donation());
-            model.addAttribute("institutions", allInstitutions);
-            model.addAttribute("allCategories", allCategories);
-        }
-
-        return "donation-form";
-    }
-
-    @PostMapping
-    private String processDonationForm(@Valid @ModelAttribute(name = "donation") Donation donation, BindingResult bindingResult, Model model) {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            String email = authentication.getName();
-            User user = userService.findUserByEmail(email);
-            UserProfile userProfile = user.getProfile();
-            model.addAttribute("userProfile", userProfile);
-            model.addAttribute("user", user);
-            donation.setUser(user);
-        }
-
-        if (bindingResult.hasErrors()) {
-            List<Category> allCategories = categoryService.findAll();
-            List<Institution> allInstitutions = institutionService.findAll();
             model.addAttribute("institutions", allInstitutions);
             model.addAttribute("allCategories", allCategories);
 
             return "donation-form";
         }
+        return "redirect:/";
+    }
 
-        donationService.save(donation);
+    @PostMapping
+    private String processDonationForm(@Valid @ModelAttribute(name = "donation") Donation donation,
+                                       BindingResult bindingResult,
+                                       @AuthenticationPrincipal CustomUserDetails userDetails,
+                                       Model model) {
+
+        if (userDetails != null) {
+
+            User user = userDetails.getUser();
+            UserProfile userProfile = user.getProfile();
+
+            model.addAttribute("user", user);
+            model.addAttribute("userProfile", userProfile);
+            donation.setUser(user);
 
 
-        return "form-confirmation";
+            if (bindingResult.hasErrors()) {
+                List<Category> allCategories = categoryService.findAll();
+                List<Institution> allInstitutions = institutionService.findAll();
+                model.addAttribute("institutions", allInstitutions);
+                model.addAttribute("allCategories", allCategories);
+
+                return "donation-form";
+            }
+
+            donationService.save(donation);
+
+            return "form-confirmation";
+        }
+        return "redirect:/";
     }
 }

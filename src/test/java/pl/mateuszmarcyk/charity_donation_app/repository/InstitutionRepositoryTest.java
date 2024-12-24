d554eb47aec6d7d5511e8a36584d443f459bf01b
mateuszmarc <mmarckiewicz@gmail.com>
2024-12-24 13:20:23 +0000

    Create InstitutionRepositoryTest Create institution-test.sql to setup data for tests

---
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/repository/InstitutionRepositoryTest.java
@@ -0,0 +1,34 @@
+package pl.mateuszmarcyk.charity_donation_app.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pl.mateuszmarcyk.charity_donation_app.entity.Institution;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+class InstitutionRepositoryTest {
+
+    @Autowired
+    private InstitutionRepository institutionRepository;
+
+
+
+    @Test
+    @Sql(scripts = "classpath:institution-test.sql")
+    void givenInstitutionRepository_whenFindAll_thenReturnListOfInstitutions() {
+        List<Institution> returnedInstitutions = institutionRepository.findAll();
+
+        assertThat(returnedInstitutions).hasSize(7);
+        }
+}
---
+++ src/test/resources/institution-test.sql
@@ -0,0 +1,7 @@
+INSERT INTO institutions (name, description) VALUES ('Pomocna Dłoń', 'Zapewniamy żywność i schronienie potrzebującym');
+INSERT INTO institutions (name, description) VALUES ('Akcja Zabawka', 'Przekazujemy zabawki dzieciom z ubogich rodzin');
+INSERT INTO institutions (name, description) VALUES ('Bank Odzieży', 'Oferujemy darmową odzież dla potrzebujących');
+INSERT INTO institutions (name, description) VALUES ('Książkowa Przystań', 'Przekazujemy książki do szkół i bibliotek w ubogich regionach');
+INSERT INTO institutions (name, description) VALUES ('Technologia dla Wszystkich', 'Zapewniamy odnowiony sprzęt elektroniczny uczniom i rodzinom');
+INSERT INTO institutions (name, description) VALUES ('Bohaterowie Higieny', 'Rozdajemy produkty higieniczne do schronisk dla bezdomnych');
+INSERT INTO institutions (name, description) VALUES ('Sport dla Dzieci', 'Wspieramy młodzieżowy sport, przekazując sprzęt i zasoby');

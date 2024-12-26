2b458faa6acdd9266f72ddc7e2376e4265c888ac
mateuszmarc <mmarckiewicz@gmail.com>
2024-12-26 08:03:30 +0000

    Email:   Add AllArgsConstructor annotation
    Create UserEmailValidatorTest

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/util/constraintannotations/Email.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/util/constraintannotations/Email.java
@@ -1,6 +1,7 @@
 package pl.mateuszmarcyk.charity_donation_app.util.constraintannotations;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Email {
 
     @NotNull(message = "{email.addressemail.notnull}")

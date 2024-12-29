fc91915a00a522179f41ec5a47aa2f6c22afe5c9
mateuszmarc <mmarckiewicz@gmail.com>
2024-12-29 09:51:47 +0000

    Create custom constructor to allow setting appEmail field without using reflection

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/util/AppMailSender.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/util/AppMailSender.java
@@ -2,7 +2,7 @@ package pl.mateuszmarcyk.charity_donation_app.util;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -11,40 +11,43 @@ import pl.mateuszmarcyk.charity_donation_app.entity.User;
 
 import java.io.UnsupportedEncodingException;
 
-@RequiredArgsConstructor
 @Component
 public class AppMailSender {
 
     private final JavaMailSender mailSender;
+    private final String appEmail;
 
-    @Value("${spring.mail.username}")
-    private String appEmail;
+    @Autowired
+    public AppMailSender(JavaMailSender mailSender, @Value("${spring.mail.username}") String appEmail) {
+        this.mailSender = mailSender;
+        this.appEmail = appEmail;
+    }
 
     public void sendEmail(User user, Mail mail) throws MessagingException, UnsupportedEncodingException {
 
-        MimeMessage message = mailSender.createMimeMessage();
-        var messageHelper = getMimeMessageHelper(message, mail);
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        var messageHelper = getMimeMessageHelper(mimeMessage, mail);
 
         messageHelper.setTo(user.getEmail());
-        mailSender.send(message);
+        mailSender.send(mimeMessage);
     }
 
     public void sendMailMessage(Mail mail) throws MessagingException, UnsupportedEncodingException {
 
-        MimeMessage message = mailSender.createMimeMessage();
-        var messageHelper = getMimeMessageHelper(message, mail);
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        var messageHelper = getMimeMessageHelper(mimeMessage, mail);
 
         messageHelper.setTo(appEmail);
 
-        mailSender.send(message);
+        mailSender.send(mimeMessage);
     }
 
-    private MimeMessageHelper getMimeMessageHelper(MimeMessage message, Mail mail) throws MessagingException, UnsupportedEncodingException {
+    private MimeMessageHelper getMimeMessageHelper(MimeMessage mimeMessage, Mail mail) throws MessagingException, UnsupportedEncodingException {
         String subject = mail.getSubject();
         String senderName = mail.getSenderName();
         String mailContent = mail.getMailContent();
 
-        var messageHelper = new MimeMessageHelper(message,true, "UTF-8");
+        var messageHelper = new MimeMessageHelper(mimeMessage,true, "UTF-8");
         messageHelper.setFrom(appEmail, senderName);
 
         messageHelper.setSubject(subject);

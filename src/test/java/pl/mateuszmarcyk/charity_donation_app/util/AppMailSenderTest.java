63e75e373f8de912eb220289c3ad7b025d71a820
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-03 18:39:38 +0000

    Create tests for AppMailsender methods when User or Mail are nulls

--- src/test/java/pl/mateuszmarcyk/charity_donation_app/util/AppMailSenderTest.java
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/util/AppMailSenderTest.java
@@ -15,6 +15,7 @@ import pl.mateuszmarcyk.charity_donation_app.entity.User;
 import java.io.UnsupportedEncodingException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,7 +39,6 @@ class AppMailSenderTest {
     @BeforeEach
     void setUp() {
         appMailSender = new AppMailSender(javaMailSender, "test@gmail.com", mimeMessageHelperFactory);
-
     }
 
     @Test
@@ -47,23 +47,72 @@ class AppMailSenderTest {
         User user = getuUser();
         Mail mail = getMail();
 
+        ArgumentCaptor<MimeMessage> mimeMessageArgumentCaptor = ArgumentCaptor.forClass(MimeMessage.class);
         ArgumentCaptor<String> emailArgumentCaptor = ArgumentCaptor.forClass(String.class);
         when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
         when(mimeMessageHelperFactory.createHelper(mimeMessage, "test@gmail.com", mail.getSenderName(), mail.getSubject(), mail.getMailContent())).thenReturn(mimeMessageHelper);
 
         appMailSender.sendEmail(user, mail);
 
-        verify(javaMailSender, times(1)).send(mimeMessage);
+        verify(javaMailSender, times(1)).send(mimeMessageArgumentCaptor.capture());
+        assertThat(mimeMessageArgumentCaptor.getValue()).isEqualTo(mimeMessage);
+
         verify(mimeMessageHelper).setTo(emailArgumentCaptor.capture());
         String setEmail = emailArgumentCaptor.getValue();
         assertThat(setEmail).isEqualTo(user.getEmail());
     }
 
+    @Test
+    void givenNullUserAndMail_whenSendEmail_thenNullPointerExceptionIsThrown() throws MessagingException, UnsupportedEncodingException {
+
+        User user = null;
+        Mail mail = getMail();
+
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+        when(mimeMessageHelperFactory.createHelper(mimeMessage, "test@gmail.com", mail.getSenderName(), mail.getSubject(), mail.getMailContent())).thenReturn(mimeMessageHelper);
+
+        assertThatThrownBy(() -> appMailSender.sendEmail(user, mail)).isInstanceOf(NullPointerException.class);
+
+        verify(javaMailSender, never()).send(mimeMessage);
+        verify(mimeMessageHelper, never()).setTo(anyString());
+    }
 
     @Test
-    void sendMailMessage() throws MessagingException, UnsupportedEncodingException {
+    void givenNullUserAndNullMail_whenSendEmail_thenNullPointerExceptionIsThrown() throws MessagingException {
+
+        User user = null;
+        Mail mail = null;
+
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        assertThatThrownBy(() -> appMailSender.sendEmail(user, mail)).isInstanceOf(NullPointerException.class);
+
+        verify(javaMailSender, times(1)).createMimeMessage();
+        verify(mimeMessageHelper, never()).setTo(anyString());
+        verify(javaMailSender, never()).send(mimeMessage);
+    }
+
+    @Test
+    void givenUserAndNullMail_whenSendEmail_thenNullPointerExceptionIsThrown() throws MessagingException {
+
+        User user = getuUser();
+        Mail mail = null;
+
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        assertThatThrownBy(() -> appMailSender.sendEmail(user, mail)).isInstanceOf(NullPointerException.class);
+
+        verify(javaMailSender, times(1)).createMimeMessage();
+        verify(mimeMessageHelper, never()).setTo(anyString());
+        verify(javaMailSender, never()).send(mimeMessage);
+    }
+
+    @Test
+    void givenMail_whenSendMailMessage_thenMessageSent() throws MessagingException, UnsupportedEncodingException {
 
         Mail mail = getMail();
+
+        ArgumentCaptor<MimeMessage> mimeMessageArgumentCaptor = ArgumentCaptor.forClass(MimeMessage.class);
         ArgumentCaptor<String> emailArgumentCaptor = ArgumentCaptor.forClass(String.class);
         when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
         when(mimeMessageHelperFactory.createHelper(mimeMessage, "test@gmail.com", mail.getSenderName(), mail.getSubject(), mail.getMailContent())).thenReturn(mimeMessageHelper);
@@ -74,9 +123,21 @@ class AppMailSenderTest {
         String setEmail = emailArgumentCaptor.getValue();
         assertThat(setEmail).isEqualTo("test@gmail.com");
 
-        verify(javaMailSender, times(1)).send(mimeMessage);
-
+        verify(javaMailSender, times(1)).send(mimeMessageArgumentCaptor.capture());
+        assertThat(mimeMessageArgumentCaptor.getValue()).isEqualTo(mimeMessage);
     }
+
+    @Test
+    void givenNullMail_whenSendMailMessage_thenNullPointerExceptionIsThrown() {
+
+        Mail mail = null;
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+       assertThatThrownBy(() -> appMailSender.sendMailMessage(mail)).isInstanceOf(NullPointerException.class);
+
+        verify(javaMailSender, never()).send(mimeMessage);
+    }
+
 
     public static Mail getMail() {
         return new Mail("Test Subject", "Test Sender", "<p>Test Content</p>");

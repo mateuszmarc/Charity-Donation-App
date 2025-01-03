0d3e3690bc0fb8499617624d238e0be3faa5d3a4
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-03 20:15:23 +0000

    MimeMessageHelperFactory: Add checks for senderName and subject: It they are null or empty then exception is thrown. Create MimeMessageHelperFactoryTest class nad tests

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/util/MimeMessageHelperFactory.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/util/MimeMessageHelperFactory.java
@@ -11,6 +11,13 @@ import java.io.UnsupportedEncodingException;
 public class MimeMessageHelperFactory {
     public MimeMessageHelper createHelper(MimeMessage mimeMessage, String from, String senderName, String subject, String content)
             throws MessagingException, UnsupportedEncodingException {
+
+        if (senderName == null || senderName.isEmpty()) {
+            throw new IllegalArgumentException("Sender name must be provided");
+        } else if (subject == null || subject.isEmpty()) {
+            throw new IllegalArgumentException("Subject must be provided");
+        }
+
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
         helper.setFrom(from, senderName);
         helper.setSubject(subject);
---
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/util/MimeMessageHelperFactoryTest.java
@@ -0,0 +1,92 @@
+package pl.mateuszmarcyk.charity_donation_app.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+class MimeMessageHelperFactoryTest {
+
+    private MimeMessageHelperFactory mimeMessageHelperFactory = new MimeMessageHelperFactory();
+
+    @Mock
+    private MimeMessage mimeMessage;
+
+
+    @Test
+    void givenNullMimeMessage_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        MimeMessage mimeMessage = null;
+        String from = "app@mail.com";
+        String senderName = "Test sender";
+        String subject = "Test subject";
+        String content = "<p>Test content</p";
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void givenMimeMessageAndNullFrom_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        String from = null;
+        String senderName = "Test sender";
+        String subject = "Test subject";
+        String content = "<p>Test content</p";
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("From address must not be null");
+    }
+
+    @Test
+    void givenMimeMessageAndNullSenderName_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        String from = "app@mail.com";
+        String senderName = null;
+        String subject = "Test subject";
+        String content = "<p>Test content</p";
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Sender name must be provided");
+    }
+
+    @Test
+    void givenMimeMessageAndEmptySenderName_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        String from = "app@mail.com";
+        String senderName = "";
+        String subject = "Test subject";
+        String content = "<p>Test content</p";
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Sender name must be provided");
+    }
+
+    @Test
+    void givenMimeMessageAndNullSubject_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        String from = "app@mail.com";
+        String senderName = "Test Sender";
+        String subject = null;
+        String content = "<p>Test content</p";
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Subject must be provided");
+    }
+
+    @Test
+    void givenMimeMessageAndEmptySubject_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        String from = "app@mail.com";
+        String senderName = "Test Sender";
+        String subject = null;
+        String content = "<p>Test content</p";
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Subject must be provided");
+    }
+
+    @Test
+    void givenMimeMessageAndNullContent_whenCreateHelper_thenNullPointerExceptionExceptionThrown() throws MessagingException, UnsupportedEncodingException {
+        String from = "app@mail.com";
+        String senderName = "Test Sender";
+        String subject = "Test subject";
+        String content = null;
+        assertThatThrownBy(() -> mimeMessageHelperFactory.createHelper(mimeMessage, from, senderName, subject, content))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Text must not be null");
+    }
+}

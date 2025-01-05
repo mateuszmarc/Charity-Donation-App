9ef66d879b648f0c2e2bf44f3abece8dd931a0a8
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-05 11:00:22 +0000

    Add tests checking that when MessagingException or UnsupportedEncodingException is thrown then they are catched and MailExcepion is thrown that is handled by AppExceptionHandler

--- src/test/java/pl/mateuszmarcyk/charity_donation_app/util/event/listener/PasswordResetEventListenerTest.java
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/util/event/listener/PasswordResetEventListenerTest.java
@@ -11,11 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 import pl.mateuszmarcyk.charity_donation_app.entity.PasswordResetVerificationToken;
 import pl.mateuszmarcyk.charity_donation_app.entity.User;
+import pl.mateuszmarcyk.charity_donation_app.exception.BusinessException;
+import pl.mateuszmarcyk.charity_donation_app.exception.MailException;
 import pl.mateuszmarcyk.charity_donation_app.service.PasswordResetVerificationTokenService;
-import pl.mateuszmarcyk.charity_donation_app.util.AppMailSender;
-import pl.mateuszmarcyk.charity_donation_app.util.Mail;
-import pl.mateuszmarcyk.charity_donation_app.util.MailMessage;
-import pl.mateuszmarcyk.charity_donation_app.util.TokenFactory;
+import pl.mateuszmarcyk.charity_donation_app.util.*;
 import pl.mateuszmarcyk.charity_donation_app.util.event.PasswordResetEvent;
 
 import java.io.UnsupportedEncodingException;
@@ -23,8 +22,8 @@ import java.time.LocalDateTime;
 import java.util.Locale;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,6 +47,9 @@ class PasswordResetEventListenerTest {
     @Mock
     private TokenFactory tokenFactory;
 
+    @Mock
+    MailFactory mailFactory;
+
     @Test
     void givenUserWithExpiredToken_whenEventPublished_thenEmailWithUpdatedTokenIsSent() throws MessagingException, UnsupportedEncodingException {
 //        Arrange
@@ -63,9 +65,9 @@ class PasswordResetEventListenerTest {
             String applicationUrl = "http://localhost/app";
             PasswordResetVerificationToken spyToken = spy(new PasswordResetVerificationToken(1L, "token", LocalDateTime.now().plusMinutes(15), spyUser, LocalDateTime.now(), false));
             spyUser.setPasswordResetVerificationToken(spyToken);
+            Mail mail = new Mail(subject, appName, message);
 
             uuidMockedStatic.when(UUID::randomUUID).thenReturn(mockUUID);
-
             when(mockUUID.toString()).thenReturn(token);
             when(messageSource.getMessage("token.valid.time", null, Locale.getDefault())).thenReturn("15");
             when(messageSource.getMessage("email.app.name", null, Locale.getDefault())).thenReturn(appName);
@@ -73,6 +75,7 @@ class PasswordResetEventListenerTest {
             when(mailMessage.buildPasswordResetMessage(expectedUrl)).thenReturn(message);
             PasswordResetEvent spyEvent = spy(new PasswordResetEvent(spyUser, applicationUrl));
             when(spyEvent.getApplicationUrl()).thenReturn(applicationUrl);
+            when(mailFactory.createMail(subject, appName, message)).thenReturn(mail);
 
 //            Act
             assertThatNoException().isThrownBy(() -> listener.onApplicationEvent(spyEvent));
@@ -104,7 +107,28 @@ class PasswordResetEventListenerTest {
             String capturedUrl = stringArgumentCaptor.getValue();
             assertThat(capturedUrl).isEqualTo(expectedUrl);
 
-            verify(appMailSender, times(1)).sendEmail(any(User.class), any(Mail.class));
+            verify(mailFactory, times(1)).createMail(stringArgumentCaptor.capture(), stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+            String capturedSubject = stringArgumentCaptor.getAllValues().get(2);
+            String capturedAppName = stringArgumentCaptor.getAllValues().get(3);
+            String capturedMessage = stringArgumentCaptor.getAllValues().get(4);
+
+            assertAll(
+                    () -> assertThat(capturedSubject).isEqualTo(subject),
+                    () -> assertThat(capturedAppName).isEqualTo(appName),
+                    () -> assertThat(capturedMessage).isEqualTo(message)
+            );
+
+            ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+            ArgumentCaptor<Mail> mailArgumentCaptor = ArgumentCaptor.forClass(Mail.class);
+            verify(appMailSender, times(1)).sendEmail(userArgumentCaptor.capture(), mailArgumentCaptor.capture());
+            User capturedUser = userArgumentCaptor.getValue();
+            Mail capturedMail = mailArgumentCaptor.getValue();
+
+
+            assertAll(
+                    () -> assertThat(capturedUser).isEqualTo(spyUser),
+                    () -> assertThat(capturedMail).isEqualTo(mail)
+            );
         }
     }
 
@@ -121,9 +145,9 @@ class PasswordResetEventListenerTest {
             User spyUser = spy(new User());
             String applicationUrl = "http://localhost/app";
             PasswordResetVerificationToken spyToken = spy(new PasswordResetVerificationToken(1L, "token", LocalDateTime.now().plusMinutes(15), spyUser, LocalDateTime.now(), false));
+            Mail mail = new Mail(subject, appName, message);
 
             uuidMockedStatic.when(UUID::randomUUID).thenReturn(mockUUID);
-
             when(mockUUID.toString()).thenReturn(token);
             when(messageSource.getMessage("token.valid.time", null, Locale.getDefault())).thenReturn("15");
             when(messageSource.getMessage("email.app.name", null, Locale.getDefault())).thenReturn(appName);
@@ -132,6 +156,7 @@ class PasswordResetEventListenerTest {
             when(mailMessage.buildPasswordResetMessage(expectedUrl)).thenReturn(message);
             PasswordResetEvent spyEvent = spy(new PasswordResetEvent(spyUser, applicationUrl));
             when(spyEvent.getApplicationUrl()).thenReturn(applicationUrl);
+            when(mailFactory.createMail(subject, appName, message)).thenReturn(mail);
 
 //            Act
             assertThatNoException().isThrownBy(() -> listener.onApplicationEvent(spyEvent));
@@ -161,7 +186,208 @@ class PasswordResetEventListenerTest {
             String capturedUrl = stringArgumentCaptor.getValue();
             assertThat(capturedUrl).isEqualTo(expectedUrl);
 
-            verify(appMailSender, times(1)).sendEmail(any(User.class), any(Mail.class));
+            verify(mailFactory, times(1)).createMail(stringArgumentCaptor.capture(), stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+            String capturedSubject = stringArgumentCaptor.getAllValues().get(1);
+            String capturedAppName = stringArgumentCaptor.getAllValues().get(2);
+            String capturedMessage = stringArgumentCaptor.getAllValues().get(3);
+
+            assertAll(
+                    () -> assertThat(capturedSubject).isEqualTo(subject),
+                    () -> assertThat(capturedAppName).isEqualTo(appName),
+                    () -> assertThat(capturedMessage).isEqualTo(message)
+            );
+
+            ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+            ArgumentCaptor<Mail> mailArgumentCaptor = ArgumentCaptor.forClass(Mail.class);
+            verify(appMailSender, times(1)).sendEmail(userArgumentCaptor.capture(), mailArgumentCaptor.capture());
+            User capturedUser = userArgumentCaptor.getValue();
+            Mail capturedMail = mailArgumentCaptor.getValue();
+
+
+            assertAll(
+                    () -> assertThat(capturedUser).isEqualTo(spyUser),
+                    () -> assertThat(capturedMail).isEqualTo(mail)
+            );
+        }
+    }
+
+    @Test
+    void whenOnApplicationEventAppMailSenderThrowsMessagingException_thenMailExceptionIsThrown() throws MessagingException, UnsupportedEncodingException {
+        try (MockedStatic<UUID> uuidMockedStatic = mockStatic(UUID.class)) {
+            String exceptionTitle = "Nie można wysłać";
+            String exceptionMessage = "Wystąpił błąd podczas wysyłania. Spróbuj ponownie";
+            String appName = "App name";
+            String subject = "Test subject";
+            String message = "Message";
+            String token = "Randomtoken";
+            int tokenValidTime = 15;
+            String expectedUrl = "http://localhost/app/reset-password/verifyEmail?token=Randomtoken";
+            UUID mockUUID = mock(UUID.class);
+            User spyUser = spy(new User());
+            String applicationUrl = "http://localhost/app";
+            PasswordResetVerificationToken spyToken = spy(new PasswordResetVerificationToken(1L, "token", LocalDateTime.now().plusMinutes(15), spyUser, LocalDateTime.now(), false));
+            Mail mail = new Mail(subject, appName, message);
+
+            uuidMockedStatic.when(UUID::randomUUID).thenReturn(mockUUID);
+            when(mockUUID.toString()).thenReturn(token);
+            when(messageSource.getMessage("token.valid.time", null, Locale.getDefault())).thenReturn("15");
+            when(messageSource.getMessage("email.app.name", null, Locale.getDefault())).thenReturn(appName);
+            when(messageSource.getMessage("registration.mail.subject", null, Locale.getDefault())).thenReturn(subject);
+            when(tokenFactory.getPasswordResetVerificationToken(token, spyUser, tokenValidTime)).thenReturn(spyToken);
+            when(mailMessage.buildPasswordResetMessage(expectedUrl)).thenReturn(message);
+            PasswordResetEvent spyEvent = spy(new PasswordResetEvent(spyUser, applicationUrl));
+            when(spyEvent.getApplicationUrl()).thenReturn(applicationUrl);
+            when(mailFactory.createMail(subject, appName, message)).thenReturn(mail);
+
+            doThrow(new MessagingException("message")).when(appMailSender).sendEmail(any(User.class), any(Mail.class));
+
+
+//            Act & Assert
+            Throwable thrown = catchThrowable(() -> listener.onApplicationEvent(spyEvent));
+            assertThat(thrown).isInstanceOf(MailException.class);
+            if (thrown instanceof BusinessException e) {
+                assertAll(
+                        () -> assertThat(e.getTitle()).isEqualTo(exceptionTitle),
+                        () -> assertThat(e.getMessage()).isEqualTo(exceptionMessage)
+                );
+            }
+
+            verify(spyEvent, times(1)).getUser();
+            verify(messageSource, times(1)).getMessage("token.valid.time", null, Locale.getDefault());
+            verify(messageSource, times(1)).getMessage("email.app.name", null, Locale.getDefault());
+            verify(messageSource, times(1)).getMessage("registration.mail.subject", null, Locale.getDefault());
+            verify(spyUser, times(1)).getPasswordResetVerificationToken();
+
+            ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+            verify(spyToken, never()).setToken(stringArgumentCaptor.capture());
+
+            verify(tokenFactory, times(1)).getPasswordResetVerificationToken(token, spyUser, tokenValidTime);
+
+            verify(spyToken, never()).setExpirationTime(any(LocalDateTime.class));
+
+            ArgumentCaptor<PasswordResetVerificationToken> passwordResetVerificationTokenArgumentCaptor = ArgumentCaptor.forClass(PasswordResetVerificationToken.class);
+            verify(passwordResetVerificationTokenService, times(1)).save(passwordResetVerificationTokenArgumentCaptor.capture());
+            PasswordResetVerificationToken capturedPasswordResetVerificationToken = passwordResetVerificationTokenArgumentCaptor.getValue();
+            assertThat(capturedPasswordResetVerificationToken).isEqualTo(spyToken);
+
+            verify(spyEvent, times(1)).getApplicationUrl();
+
+            verify(mailMessage, times(1)).buildPasswordResetMessage(stringArgumentCaptor.capture());
+            String capturedUrl = stringArgumentCaptor.getValue();
+            assertThat(capturedUrl).isEqualTo(expectedUrl);
+
+            verify(mailFactory, times(1)).createMail(stringArgumentCaptor.capture(), stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+            String capturedSubject = stringArgumentCaptor.getAllValues().get(1);
+            String capturedAppName = stringArgumentCaptor.getAllValues().get(2);
+            String capturedMessage = stringArgumentCaptor.getAllValues().get(3);
+
+            assertAll(
+                    () -> assertThat(capturedSubject).isEqualTo(subject),
+                    () -> assertThat(capturedAppName).isEqualTo(appName),
+                    () -> assertThat(capturedMessage).isEqualTo(message)
+            );
+
+            ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+            ArgumentCaptor<Mail> mailArgumentCaptor = ArgumentCaptor.forClass(Mail.class);
+            verify(appMailSender, times(1)).sendEmail(userArgumentCaptor.capture(), mailArgumentCaptor.capture());
+            User capturedUser = userArgumentCaptor.getValue();
+            Mail capturedMail = mailArgumentCaptor.getValue();
+
+
+            assertAll(
+                    () -> assertThat(capturedUser).isEqualTo(spyUser),
+                    () -> assertThat(capturedMail).isEqualTo(mail)
+            );
+        }
+    }
+
+    @Test
+    void whenOnApplicationEventAppMailSenderThrowsUnsupportedEncodingException_thenMailExceptionIsThrown() throws MessagingException, UnsupportedEncodingException {
+        try (MockedStatic<UUID> uuidMockedStatic = mockStatic(UUID.class)) {
+            String exceptionTitle = "Nie można wysłać";
+            String exceptionMessage = "Wystąpił błąd podczas wysyłania. Spróbuj ponownie";
+            String appName = "App name";
+            String subject = "Test subject";
+            String message = "Message";
+            String token = "Randomtoken";
+            int tokenValidTime = 15;
+            String expectedUrl = "http://localhost/app/reset-password/verifyEmail?token=Randomtoken";
+            UUID mockUUID = mock(UUID.class);
+            User spyUser = spy(new User());
+            String applicationUrl = "http://localhost/app";
+            PasswordResetVerificationToken spyToken = spy(new PasswordResetVerificationToken(1L, "token", LocalDateTime.now().plusMinutes(15), spyUser, LocalDateTime.now(), false));
+            Mail mail = new Mail(subject, appName, message);
+
+            uuidMockedStatic.when(UUID::randomUUID).thenReturn(mockUUID);
+            when(mockUUID.toString()).thenReturn(token);
+            when(messageSource.getMessage("token.valid.time", null, Locale.getDefault())).thenReturn("15");
+            when(messageSource.getMessage("email.app.name", null, Locale.getDefault())).thenReturn(appName);
+            when(messageSource.getMessage("registration.mail.subject", null, Locale.getDefault())).thenReturn(subject);
+            when(tokenFactory.getPasswordResetVerificationToken(token, spyUser, tokenValidTime)).thenReturn(spyToken);
+            when(mailMessage.buildPasswordResetMessage(expectedUrl)).thenReturn(message);
+            PasswordResetEvent spyEvent = spy(new PasswordResetEvent(spyUser, applicationUrl));
+            when(spyEvent.getApplicationUrl()).thenReturn(applicationUrl);
+            when(mailFactory.createMail(subject, appName, message)).thenReturn(mail);
+
+            doThrow(new UnsupportedEncodingException("message")).when(appMailSender).sendEmail(any(User.class), any(Mail.class));
+
+
+//            Act & Assert
+            Throwable thrown = catchThrowable(() -> listener.onApplicationEvent(spyEvent));
+            assertThat(thrown).isInstanceOf(MailException.class);
+            if (thrown instanceof BusinessException e) {
+                assertAll(
+                        () -> assertThat(e.getTitle()).isEqualTo(exceptionTitle),
+                        () -> assertThat(e.getMessage()).isEqualTo(exceptionMessage)
+                );
+            }
+
+            verify(spyEvent, times(1)).getUser();
+            verify(messageSource, times(1)).getMessage("token.valid.time", null, Locale.getDefault());
+            verify(messageSource, times(1)).getMessage("email.app.name", null, Locale.getDefault());
+            verify(messageSource, times(1)).getMessage("registration.mail.subject", null, Locale.getDefault());
+            verify(spyUser, times(1)).getPasswordResetVerificationToken();
+
+            ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+            verify(spyToken, never()).setToken(stringArgumentCaptor.capture());
+
+            verify(tokenFactory, times(1)).getPasswordResetVerificationToken(token, spyUser, tokenValidTime);
+
+            verify(spyToken, never()).setExpirationTime(any(LocalDateTime.class));
+
+            ArgumentCaptor<PasswordResetVerificationToken> passwordResetVerificationTokenArgumentCaptor = ArgumentCaptor.forClass(PasswordResetVerificationToken.class);
+            verify(passwordResetVerificationTokenService, times(1)).save(passwordResetVerificationTokenArgumentCaptor.capture());
+            PasswordResetVerificationToken capturedPasswordResetVerificationToken = passwordResetVerificationTokenArgumentCaptor.getValue();
+            assertThat(capturedPasswordResetVerificationToken).isEqualTo(spyToken);
+
+            verify(spyEvent, times(1)).getApplicationUrl();
+
+            verify(mailMessage, times(1)).buildPasswordResetMessage(stringArgumentCaptor.capture());
+            String capturedUrl = stringArgumentCaptor.getValue();
+            assertThat(capturedUrl).isEqualTo(expectedUrl);
+
+            verify(mailFactory, times(1)).createMail(stringArgumentCaptor.capture(), stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+            String capturedSubject = stringArgumentCaptor.getAllValues().get(1);
+            String capturedAppName = stringArgumentCaptor.getAllValues().get(2);
+            String capturedMessage = stringArgumentCaptor.getAllValues().get(3);
+
+            assertAll(
+                    () -> assertThat(capturedSubject).isEqualTo(subject),
+                    () -> assertThat(capturedAppName).isEqualTo(appName),
+                    () -> assertThat(capturedMessage).isEqualTo(message)
+            );
+
+            ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+            ArgumentCaptor<Mail> mailArgumentCaptor = ArgumentCaptor.forClass(Mail.class);
+            verify(appMailSender, times(1)).sendEmail(userArgumentCaptor.capture(), mailArgumentCaptor.capture());
+            User capturedUser = userArgumentCaptor.getValue();
+            Mail capturedMail = mailArgumentCaptor.getValue();
+
+
+            assertAll(
+                    () -> assertThat(capturedUser).isEqualTo(spyUser),
+                    () -> assertThat(capturedMail).isEqualTo(mail)
+            );
         }
     }
 }

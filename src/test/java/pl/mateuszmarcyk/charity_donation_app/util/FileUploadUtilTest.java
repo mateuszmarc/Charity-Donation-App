8efcb55f4fe750351ec9341c709b95eba19e1646
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-05 09:52:10 +0000

    Crete two more test methods to test IOException thrown by saveImage and saveFile methods

--- src/test/java/pl/mateuszmarcyk/charity_donation_app/util/FileUploadUtilTest.java
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/util/FileUploadUtilTest.java
@@ -1,11 +1,13 @@
 package pl.mateuszmarcyk.charity_donation_app.util;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +17,13 @@ import pl.mateuszmarcyk.charity_donation_app.service.UserService;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Comparator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,6 +46,26 @@ class FileUploadUtilTest {
         tempDir = Files.createTempDirectory("test-upload-dir");
     }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        deleteDirectoryRecursively(tempDir);
+        deleteDirectoryRecursively(Path.of("test-dir"));
+    }
+
+    private void deleteDirectoryRecursively(Path path) throws IOException {
+        if (Files.exists(path)) {
+            Files.walk(path)
+                    .sorted(Comparator.reverseOrder())
+                    .forEach(p -> {
+                        try {
+                            Files.delete(p);
+                        } catch (IOException e) {
+                            throw new RuntimeException("Failed to delete: " + p, e);
+                        }
+                    });
+        }
+    }
+
     @Test
     void givenFileUploadUtil_whenSaveFile_thenFileIsSaved() throws IOException {
         String filename = "test-file.txt";
@@ -53,6 +77,35 @@ class FileUploadUtilTest {
         Path savedFile = tempDir.resolve(filename);
         assertThat(Files.exists(savedFile)).isTrue();
         assertThat(Files.readAllBytes(savedFile)).isEqualTo(fileContent);
+    }
+
+    @Test
+    void givenFileUploadUtilAndNoDirectory_whenSaveFile_thenDirectoryCreatedAndFileIsSaved() throws IOException {
+        String filename = "test-file.txt";
+        Path notExistingDirectory = Path.of("test-dir");
+        byte[] fileContent = "Test Content".getBytes();
+        when(multipartFile.getInputStream()).thenReturn(new java.io.ByteArrayInputStream(fileContent));
+
+        fileUploadUtil.saveFile(notExistingDirectory.toString(), filename, multipartFile);
+
+        Path savedFile = notExistingDirectory.resolve(filename);
+        assertThat(Files.exists(savedFile)).isTrue();
+        assertThat(Files.readAllBytes(savedFile)).isEqualTo(fileContent);
+    }
+
+    @Test
+    void givenFileUploadUtil_whenSaveFileAndIOExceptionThrown_thenAnotherIOExceptionThrown() throws IOException {
+        String filename = "test-file.txt";
+        byte[] fileContent = "Test Content".getBytes();
+
+        when(multipartFile.getInputStream()).thenReturn(new java.io.ByteArrayInputStream(fileContent));
+
+        try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+
+            filesMockedStatic.when(() -> Files.copy(any(InputStream.class), any(Path.class), any(StandardCopyOption.class))).thenThrow(IOException.class);
+
+            assertThatThrownBy(() -> fileUploadUtil.saveFile(tempDir.toString(), filename, multipartFile)).isInstanceOf(IOException.class).hasMessage("Could not save image file: test-file.txt");
+        }
     }
 
     @Test
@@ -87,7 +140,27 @@ class FileUploadUtilTest {
     }
 
     @Test
-    void givenFileUploadUtil_whenSaveImageWithEmptyFilename_thenNoFileSaved() {
+    void givenFileUploadUtil_whenSaveImageAndSaveFileThrowsException_thenImageIsNotSaved() throws IOException {
+        try (MockedStatic<Files> filesMockedStatic = mockStatic(Files.class)) {
+            filesMockedStatic.when(() -> Files.copy(any(InputStream.class), any(Path.class), any(StandardCopyOption.class))).thenThrow(IOException.class);
+
+            UserProfile profile = spy(new UserProfile());
+            User user = spy(new User());
+            user.setId(1L);
+            String originalFilename = "image.jpg";
+
+            when(multipartFile.getOriginalFilename()).thenReturn(originalFilename);
+            when(multipartFile.getInputStream()).thenReturn(new ByteArrayInputStream("image content".getBytes()));
+
+            assertThatThrownBy(() -> fileUploadUtil.saveImage(profile, multipartFile, user)).isInstanceOf(IOException.class);
+            Path path = Path.of("photos/users/1/");
+            Path savedImage = path.resolve(originalFilename);
+            assertThat(Files.exists(savedImage)).isFalse();
+        }
+    }
+
+    @Test
+    void givenFileUploadUtil_whenSaveImageWithEmptyFilename_thenNoFileSaved() throws IOException {
         UserProfile profile = mock(UserProfile.class);
         User user = new User();
 
@@ -99,5 +172,6 @@ class FileUploadUtilTest {
         verify(userService, times(1)).updateUser(user);
         assertThat(profile.getProfilePhoto()).isNull();
     }
+
 }
 

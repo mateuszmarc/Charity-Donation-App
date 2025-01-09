6bd5bf0434b7aae8b61995ddd09706feccb8868b
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-09 13:57:13 +0000

    Add Slf4j annotation. Change all print statements to user slf4j. Change isValid method to be more readable - add log statements.

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/util/constraintannotations/UniqueEmailConstraintValidator.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/util/constraintannotations/UniqueEmailConstraintValidator.java
@@ -3,6 +3,7 @@ package pl.mateuszmarcyk.charity_donation_app.util.constraintannotations;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.mateuszmarcyk.charity_donation_app.entity.User;
@@ -10,6 +11,7 @@ import pl.mateuszmarcyk.charity_donation_app.repository.UserRepository;
 
 import java.util.Optional;
 
+@Slf4j
 @Component
 @NoArgsConstructor
 public class UniqueEmailConstraintValidator implements ConstraintValidator<UniqueEmail, User> {
@@ -28,24 +30,35 @@ public class UniqueEmailConstraintValidator implements ConstraintValidator<Uniqu
     }
 
     @Override
-    public boolean isValid(User user, ConstraintValidatorContext constraintValidatorContext) {
-        if (user.getEmail() == null) {
+    public boolean isValid(User userToValidate, ConstraintValidatorContext constraintValidatorContext) {
+        if (userToValidate.getEmail() == null) {
             return true;
         }
 
-        System.out.println("user id " + user.getId());
+        log.info("Validated user id {}", userToValidate.getId());
+        log.info("Validated user email  ={}", userToValidate.getEmail());
 
 
-        Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
+        Optional<User> optionalUser = userRepository.findByEmail(userToValidate.getEmail());
         if (optionalUser.isPresent()) {
             User foundUser = optionalUser.get();
-            System.out.println("Found user id " + foundUser.getId());
-            if (user.getId() == null) {
+            log.info("Found user id {}", foundUser.getId());
+            log.info("Found user email {}", foundUser.getEmail());
+            if (userToValidate.getId() == null) {
+               log.info("Email is invalid");
                 return false;
-            } else return user.getId().equals(foundUser.getId());
-
+            } else {
+                if (userToValidate.getId().equals(foundUser.getId())) {
+                   log.info("User is updating his own email");
+                    return true;
+                } else {
+                   log.info("They are different users - one user is in database with such email already.");
+                    return false;
+                }
+            }
         }
         return true;
     }
-
 }
+
+

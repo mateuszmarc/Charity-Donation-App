23edb7a47fa05fde18647b6862a034f9a9b7d931
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-10 09:13:43 +0000

    Change index method: - Remove statements adding institutions, donationCount and donationsBagCount attributes to the model - they are annotated with ModelAttribute annotations. - Add statement adding MessageDTO instance to the model.
    Change processMessageForm method:
    - Use MessageDTO and Validators to validate message.

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/controller/HomeController.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/controller/HomeController.java
@@ -1,41 +1,54 @@
 package pl.mateuszmarcyk.charity_donation_app.controller;
 
 import jakarta.mail.MessagingException;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.mateuszmarcyk.charity_donation_app.config.security.CustomUserDetails;
 import pl.mateuszmarcyk.charity_donation_app.entity.Institution;
 import pl.mateuszmarcyk.charity_donation_app.entity.User;
 import pl.mateuszmarcyk.charity_donation_app.service.DonationService;
 import pl.mateuszmarcyk.charity_donation_app.service.InstitutionService;
-import pl.mateuszmarcyk.charity_donation_app.service.UserService;
-import pl.mateuszmarcyk.charity_donation_app.util.AppMailSender;
-import pl.mateuszmarcyk.charity_donation_app.util.LoggedUserModelHandler;
-import pl.mateuszmarcyk.charity_donation_app.util.Mail;
-import pl.mateuszmarcyk.charity_donation_app.util.MailMessage;
+import pl.mateuszmarcyk.charity_donation_app.util.*;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Locale;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 
-    private final UserService userService;
     private final DonationService donationService;
     private final InstitutionService institutionService;
     private final AppMailSender appMailSender;
     private final MailMessage mailMessageHelper;
     private final LoggedUserModelHandler loggedUserModelHandler;
+    private final MessageSource messageSource;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
 
     @GetMapping("/")
     public String index(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+
+        MessageDTO messageDTO = new MessageDTO();
 
         if (userDetails != null) {
             if (userDetails.getAuthorities().stream().noneMatch(authority -> authority.getAuthority().equals("ROLE_USER"))) {
@@ -43,6 +56,8 @@ public class HomeController {
             }
             User loggedUser = loggedUserModelHandler.getUser(userDetails);
             loggedUserModelHandler.addUserToModel(loggedUser, model);
+
+            messageDTO.setEmail(loggedUser.getEmail());
         }
 
         List<Institution> institutions = institutionService.findAll();
@@ -52,50 +67,58 @@ public class HomeController {
         model.addAttribute("institutions", institutions);
         model.addAttribute("allDonations", allDonations);
         model.addAttribute("allDonationBags", allDonationBags);
+        model.addAttribute("message", messageDTO);
+
 
         return "index";
     }
 
     @PostMapping("/message")
-    public String processMessageForm(@AuthenticationPrincipal CustomUserDetails userDetails, HttpServletRequest request, Model model) {
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
-        String message = request.getParameter("message");
-        String messageEmail = request.getParameter("email");
-        User user = null;
+    public String processMessageForm(@AuthenticationPrincipal CustomUserDetails userDetails, @Valid @ModelAttribute(name = "message") MessageDTO message, BindingResult bindingResult, Model model) {
 
-        String infoMessage = "Wiadomość nie została wysłana. Wszystkie pola muszą być wypełnione. Kliknij, a zostaniesz przekierowany do formularza";
-
+        String messageSuccessInfo = messageSource.getMessage("mail.message.success.info", null, Locale.getDefault());
         if (userDetails != null) {
-            user = loggedUserModelHandler.getUser(userDetails);
+            User user = loggedUserModelHandler.getUser(userDetails);
             loggedUserModelHandler.addUserToModel(user, model);
-            messageEmail = user.getEmail();
-
         }
 
-        if (validateMailMessage(firstName, lastName, message, messageEmail)) {
-            String mailMessage = mailMessageHelper.getMailMessage(firstName, lastName, message, user);
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(error -> log.info("{}", error));
+            return "index";
+        }
+
+        String firstName = message.getFirstName();
+        String lastName = message.getLastName();
+        String messageContent = message.getMessage();
+        String email = message.getEmail();
+
+        String mailMessage = mailMessageHelper.getMailMessage(firstName, lastName, messageContent, email);
             Mail mail = new Mail("Nowa wiadomość", firstName + " " + lastName, mailMessage);
 
             try {
                 appMailSender.sendMailMessage(mail);
-                infoMessage = "Wiadomość wysłana pomyślnie. Odpowiemy tak szybko jak tylko możemy";
-                model.addAttribute("messageSuccess", infoMessage);
             } catch (MessagingException | UnsupportedEncodingException e) {
                 throw new RuntimeException(e);
             }
-        } else {
-            model.addAttribute("messageError", infoMessage);
-        }
+
+        model.addAttribute("messageSuccess", messageSuccessInfo);
+
         return "index";
     }
 
-    private boolean validateMailMessage(String firstName, String lastName, String message, String messageEmail) {
+    @ModelAttribute(name = "donations")
+    public List<Institution> getInstitutions() {
+        return institutionService.findAll();
+    }
 
-        return firstName != null && !firstName.trim().isEmpty() &&
-                lastName != null && !lastName.trim().isEmpty() &&
-                message != null && !message.trim().isEmpty() &&
-                messageEmail != null && !messageEmail.trim().isEmpty();
+    @ModelAttribute(name = "allDonations")
+    public Integer getDonationCount() {
+        return donationService.countAllDonations();
+    }
+
+    @ModelAttribute(name = "allDonationBags")
+    public Integer getDonationBagCount() {
+        return donationService.countAllBags();
     }
 
 }

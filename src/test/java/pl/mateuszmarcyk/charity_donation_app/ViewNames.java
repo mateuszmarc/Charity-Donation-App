2d395b80337a444aabfbe18a9c6b987550cabae5
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-13 18:42:32 +0000

    Enter all views used in AdminController

--- src/test/java/pl/mateuszmarcyk/charity_donation_app/ViewNames.java
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/ViewNames.java
@@ -2,4 +2,19 @@ package pl.mateuszmarcyk.charity_donation_app;
 
 public class ViewNames {
     public static final String ADMIN_DASHBOARD_VIEW = "admin-dashboard";
+    public static final String ALL_ADMINS_AND_USERS_VIEW = "admin-users-all";
+    public static final String ADMIN_USER_ACCOUNT_DETAILS_VIEW = "admin-user-account-details";
+    public static final String ADMIN_USER_PROFILE_DETAILS_VIEW = "admin-user-profile-details";
+    public static final String ADMIN_USER_PROFILE_DETAILS_FORM_VIEW = "admin-user-profile-details-form";
+    public static final String ADMIN_USERS_ACCOUNT_FORM_VIEW = "admin-user-account-edit-form";
+    public static final String ADMIN_DONATIONS_ALL_VIEW = "admin-donations-all";
+    public static final String ADMIN_DONATIONS_DETAILS_VIEW = "admin-donation-details";
+    public static final String ADMIN_CATEGORY_FORM_VIEW = "admin-category-form";
+    public static final String ADMIN_CATEGORY_DETAILS_VIEW = "admin-category-details";
+    public static final String ADMIN_CATEGORIES_ALL_VIEW = "admin-categories-all";
+    public static final String ADMIN_INSTITUTIONS_ALL_VIEW = "admin-institutions-all";
+    public static final String ADMIN_INSTITUTION_DETAILS_VIEW = "admin-institution-details";
+    public static final String ADMIN_INSTITUTION_FORM_VIEW = "admin-institution-form";
+
+    public static final String ERROR_PAGE_VIEW = "error-page";
 }

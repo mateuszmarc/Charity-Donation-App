29e747561e82f621ef061568b826d14410ca0940
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-13 18:43:04 +0000

    Enter all endpoints used in AdminController

--- src/test/java/pl/mateuszmarcyk/charity_donation_app/UrlTemplates.java
+++ src/test/java/pl/mateuszmarcyk/charity_donation_app/UrlTemplates.java
@@ -4,6 +4,38 @@ public class UrlTemplates {
     public static final String ACCESS_DENIED_URL = "/error/403";
     public static final String APPLICATION_URL = "/app";
     public static final String APP_ADMIN_DASHBOARD_URL = "/app/admins/dashboard";
-    public static final String ADMIN_DASHBOARD_URL = "/app/admins/dashboard";
+    public static final String ADMIN_DASHBOARD_URL = "/admins/dashboard";
+    public static final String ADMIN_ALL_ADMINS_URL = "/admins/all-admins";
+    public static final String ADMIN_ALL_USERS_URL = "/admins/users";
+    public static final String ADMIN_USER_ACCOUNT_DETAILS_URL = "/admins/users/{id}";
+    public static final String ADMIN_USER_PROFILE_DETAILS_URL = "/admins/users/profiles/{id}";
+    public static final String ADMIN_USER_PROFILE_DETAILS_EDIT_URL = "/admins/users/profiles/edit/{id}";
+    public static final String ADMIN_USER_PROFILE_DETAILS_EDIT_POST_URL = "/admins/users/profiles/edit";
+    public static final String ADMIN_USERS_PROFILES_URL = "/admins/users/profiles";
+    public static final String ADMIN_USERS_ACCOUNT_EDIT_FORM_URL = "/admins/users/edit/{id}";
+    public static final String ADMIN_USERS_EMAIL_CHANGE_URL = "/admins/users/change-email";
+    public static final String ADMIN_USERS_PASSWORD_CHANGE_URL = "/admins/users/change-password";
+    public static final String ADMIN_USERS_BLOCK_URL = "/admins/users/block/{id}";
+    public static final String ADMIN_USERS_UNBLOCK_URL = "/admins/users/unblock/{id}";
+    public static final String ADMIN_USERS_UPGRADE_URL = "/admins/users/upgrade/{id}";
+    public static final String ADMIN_USERS_DOWNGRADE_URL = "/admins/users/downgrade/{id}";
+    public static final String ADMIN_USERS_DELETE_URL = "/admins/users/delete";
+    public static final String ADMIN_DONATIONS_URL = "/admins/donations";
+    public static final String ADMIN_DONATIONS_ARCHIVE_URL = "/admins/donations/archive";
+    public static final String ADMIN_DONATIONS_UN_ARCHIVE_URL = "/admins/donations/unarchive";
+    public static final String ADMIN_DONATIONS_DELETE_URL = "/admins/donations/delete";
+    public static final String ADMIN_DONATIONS_DONATION_DETAILS_URL = "/admins/donations/{id}";
+    public static final String ADMIN_CATEGORIES_URL = "/admins/categories";
+    public static final String ADMIN_CATEGORIES_DETAILS_URL = "/admins/categories/{categoryId}";
+    public static final String ADMIN_CATEGORIES_ADD_URL = "/admins/categories/add";
+    public static final String ADMIN_CATEGORIES_EDIT_URL = "/admins/categories/edit/{id}";
+    public static final String ADMIN_CATEGORIES_DELETE_URL = "/admins/categories/delete";
+    public static final String ADMIN_INSTITUTIONS_URL = "/admins/institutions";
+    public static final String ADMIN_INSTITUTIONS_DETAILS_URL = "/admins/institutions/{id}";
+    public static final String ADMIN_INSTITUTIONS_ADD_URL = "/admins/institutions/add";
+    public static final String ADMIN_INSTITUTIONS_EDIT_URL = "/admins/institutions/edit/{id}";
+    public static final String ADMIN_INSTITUTIONS_DELETE_URL = "/admins/institutions/delete";
+
+
 
 }

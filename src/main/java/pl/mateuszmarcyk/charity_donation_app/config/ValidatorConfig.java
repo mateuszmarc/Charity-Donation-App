8648c123a26eeba98ed788e363678951151d1381
mateuszmarc <mmarckiewicz@gmail.com>
2025-01-15 15:29:07 +0000

    ValidatorConfig:   - setValidationMessageSource in LocalValidatorFactory bean:   - Adjust VelidatorConfig test.

--- src/main/java/pl/mateuszmarcyk/charity_donation_app/config/ValidatorConfig.java
+++ src/main/java/pl/mateuszmarcyk/charity_donation_app/config/ValidatorConfig.java
@@ -1,10 +1,10 @@
 package pl.mateuszmarcyk.charity_donation_app.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import pl.mateuszmarcyk.charity_donation_app.util.constraintannotations.SpringConstraintValidatorFactory;
-
 
 @Configuration
 public class ValidatorConfig {
@@ -16,8 +16,9 @@ public class ValidatorConfig {
     }
 
     @Bean
-    public LocalValidatorFactoryBean validatorFactoryBean() {
+    public LocalValidatorFactoryBean validatorFactoryBean(MessageSource messageSource) {
         LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+        factoryBean.setValidationMessageSource(messageSource);
         factoryBean.setConstraintValidatorFactory(constraintValidatorFactory);
         return factoryBean;
     }
